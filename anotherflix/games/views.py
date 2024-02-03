13ca07bc40c8dea4eef2eb79dbfb0a9994e63dd9
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2024-02-02 22:28:38 -0300

    *

--- anotherflix/games/views.py
+++ anotherflix/games/views.py
@@ -1,12 +1,16 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Game
 from .serializers import GameSerializer
 
 
-def game_list(request):
+def game_list(request: HttpRequest) -> HttpResponse:
     games = Game.objects.all()
-    return render(request, 'games/game_list.html', {'games': games})
+    context = {
+        'games': games
+    }
+    return render(request, 'games/game_list.html', context)
 
 class GameViewSet(viewsets.ModelViewSet):
     queryset = Game.objects.all()
